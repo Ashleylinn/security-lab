05a599754f5fd29cbe52c9e5c9cd727b766e7e95
Ashleylinn <ashleylin1016@gmail.com>
2026-01-24 16:23:12 -0500

    modified config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,12 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "ashleylin",
-  database: "security_lab"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
